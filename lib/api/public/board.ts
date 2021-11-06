df890fb5d6d0515630f682d7a5b6ae61b0ed97d1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-07 00:17:17 +0900

    :white_check_mark:  Add `fetchBoard` test case

--- lib/api/public/board.ts
+++ lib/api/public/board.ts
@@ -3,32 +3,26 @@ import { jsonFetch } from '@/shared/fetch'
 import type { BitflyerPair } from '@/shared/types/currency'
 import type { PublicAPI } from '@/shared/types/fetch'
 
-type MarketsOptions = {
+type BoardOptions = {
   productCode: BitflyerPair
 }
 
-type Lot = {
-  price: number
-  size: number
-}
-
-type MarketsResponse = {
+type BoardResponse = {
   mid_price: number
-  bids: Lot[]
-  asks: Lot[]
+  bids: { price: number; size: number }[]
+  asks: { price: number; size: number }[]
 }
 
-const fetchBoard: PublicAPI<MarketsOptions, MarketsResponse> = (
+const fetchBoard: PublicAPI<BoardOptions, BoardResponse> = (
   { productCode },
   init
 ) => {
   const url = new URL(BOARD, BASE_URL)
 
-  url.search = new URLSearchParams({
-    product_code: productCode
-  }).toString()
+  url.searchParams.set('product_code', productCode)
 
   return jsonFetch(url, init)
 }
 
 export { fetchBoard }
+export type { BoardOptions, BoardResponse }
--- lib/api/public/index.ts
+++ lib/api/public/index.ts
@@ -1,7 +1,9 @@
 export { fetchTicker } from '@/api/public/ticker'
 export type { TickerOptions, TickerResponse } from '@/api/public/ticker'
 
-export * from '@/api/public/board'
+export { fetchBoard } from '@/api/public/board'
+export type { BoardResponse, BoardOptions } from '@/api/public/board'
+
 export { fetchBoardState } from '@/api/public/board_state'
 export type {
   BoardStateOptions,
